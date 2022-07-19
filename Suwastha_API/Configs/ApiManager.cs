88d6d65c35420e40cbfbcd33081672cb1e0d4c83
Hirushan <hirushan@app360.lk>
2022-07-19 23:33:29 +0530

    created APIManager with ConnectionString

---
+++ Suwastha_API/Configs/ApiManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+namespace POS360_API.Configs
+{
+    public sealed class ApiManager
+    {
+
+        private ApiManager() { }
+        private static ApiManager instance = null;
+        public static ApiManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ApiManager();
+                }
+                return instance;
+            }
+        }
+
+
+        public SqlConnectionStringBuilder GetConnectionString()
+        {
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-IMGD37M\SQLEXPRESS";  
+            builder.UserID = "Hirushan";         
+            builder.Password = "hirushan";  
+            builder.InitialCatalog = "Suwastha_DB";
+            return builder;
+        }
+    }
+
+
+    enum APIResponse
+    {
+        Success,
+        Fail
+    }
+}
