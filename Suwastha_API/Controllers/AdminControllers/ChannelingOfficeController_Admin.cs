97cf577b5e85d3478daaf3cecf3db1ed5728e2d8
Hirushan-N <nadeeshhirushan@gmail.com>
2022-07-31 13:40:20 +0530

    created ChannelingOffice controller for admin

---
+++ Suwastha_API/Controllers/AdminControllers/ChannelingOfficeController_Admin.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Suwastha_API.Models;
+using Suwastha_API.Services;
+
+namespace Suwastha_API.Controllers.ChannelingOfficeControllers
+{
+    [ApiController]
+    [Route("api/v1/Admin/ChannelingOffice")]
+    public class ChannelingOfficeController_Admin : Controller
+    {
+        [HttpPost]
+        public ResponseResult Create([FromBody]ChannelingOffice channelingOffice)
+        {
+            channelingOffice.setChannelingOfficeID();
+            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
+            return channelingOfficeService.Save(channelingOffice);
+        }
+
+        [HttpPut]
+        public ResponseResult Update([FromBody]ChannelingOffice channelingOffice)
+        {
+            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
+            return channelingOfficeService.Save(channelingOffice);
+        }
+
+        [HttpPut]
+        [Route("Delete")]
+        public ResponseResult Delete([FromBody]ChannelingOffice channelingOffice)
+        {
+            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
+            return channelingOfficeService.Delete(channelingOffice);
+        }
+    }
+}
--- Suwastha_API/Controllers/ChannelingOfficeControllers/ChannelingOfficeController_COF.cs
+++ Suwastha_API/Controllers/ChannelingOfficeControllers/ChannelingOfficeController_COF.cs
@@ -9,14 +9,6 @@ namespace Suwastha_API.Controllers.ChannelingOfficeControllers
     public class ChannelingOfficeController_COF : Controller
     {
         [HttpPost]
-        public ResponseResult Create([FromBody]ChannelingOffice channelingOffice)
-        {
-            channelingOffice.setChannelingOfficeID();
-            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
-            return channelingOfficeService.Save(channelingOffice);
-        }
-
-        [HttpPost]
         [Route("Login")]
         public ResponseResult Login([FromBody]LoginRequest loginRequest)
         {
@@ -24,19 +16,5 @@ namespace Suwastha_API.Controllers.ChannelingOfficeControllers
             return channelingOfficeService.Login(loginRequest);
         }
 
-        [HttpPut]
-        public ResponseResult Update([FromBody]ChannelingOffice channelingOffice)
-        {
-            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
-            return channelingOfficeService.Save(channelingOffice);
-        }
-
-        [HttpPut]
-        [Route("Delete")]
-        public ResponseResult Delete([FromBody]ChannelingOffice channelingOffice)
-        {
-            ChannelingOfficeService channelingOfficeService = new ChannelingOfficeService();
-            return channelingOfficeService.Delete(channelingOffice);
-        }
     }
 }
